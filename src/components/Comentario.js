a0002ee6d1d65b13d075e15799f3e72a718b2023
JorgeRudders05 <Jaclscode@email.com>
2023-09-13 11:47:18 -0300

    Construindo componentes

---
+++ src/components/Comentario.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Comentario.css'
+
+const Comentario = () => {
+  return (
+    <div className="Comentario">
+      <div>Jorge:</div>
+      <div>Olá, tudo bem?</div>
+      
+    </div>
+  );
+};
+
+export default Comentario;
